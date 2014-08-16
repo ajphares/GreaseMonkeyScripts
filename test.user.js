6aaf2f260522021eed377d593a630bb045674242
Andrew Phares <pharesa@gmail.com>
2014-08-16 11:38:46 -0700

    First commit!

---
+++ test.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name		Auto Updating Test
+// @grant		none
+// @include		*espn*
+// @version		0.1
+// ==/UserScript==
+
+alert('This is a GreaseMonkey Script running on espn.com');
