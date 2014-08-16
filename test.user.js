964ba2d4554390242c1480452cae08e267565d51
Andrew Phares <pharesa@gmail.com>
2014-08-16 11:54:13 -0700

    Adding downloadURL metadata

--- test.user.js
+++ test.user.js
@@ -3,6 +3,7 @@
 // @grant		none
 // @include		*espn*
 // @version		0.2
+// @downloadURL	https://github.com/ajphares/GreaseMonkeyScripts/raw/master/test.user.js
 // ==/UserScript==
 
 console.log('This is a less annoying GreaseMonkey Script running on espn.com');
