c9b4f28becc34d06e60211343cbd4abf7ad181d4
Andrew Phares <pharesa@gmail.com>
2014-08-16 11:57:36 -0700

    New version

--- test.user.js
+++ test.user.js
@@ -2,8 +2,9 @@
 // @name			Auto Updating Test
 // @grant			none
 // @include			*espn*
-// @version			0.2
+// @version			0.3
 // @downloadURL		https://github.com/ajphares/GreaseMonkeyScripts/raw/master/test.user.js
 // ==/UserScript==
 
 console.log('This is a less annoying GreaseMonkey Script running on espn.com');
+console.log('Here I am outputting more nonsense to the console.');
